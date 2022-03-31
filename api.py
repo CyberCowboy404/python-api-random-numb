11289c2a822be2a32425f5a1facefef888728323
CyberCowboy404 <cybercowboy404@gmail.com>
2022-03-31 18:53:05 +0300

    inited project and api

---
+++ api.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import random
+
+app = Flask(__name__)
+api = Api(app)
+
+class RandomNumberApi(Resource):
+    def get(self):
+        range = request.args.get('range')
+        if range and range.isnumeric():
+            return random.randint(1, int(range))
+        else:
+            return random.randint(1, 1000)
+
+api.add_resource(RandomNumberApi, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
